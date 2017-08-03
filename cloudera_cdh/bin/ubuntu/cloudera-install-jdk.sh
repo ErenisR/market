23044308b800cce3df79f62179aeb403bf90a8d1
ErenisR <erenisramadani@gmail.com>
2017-08-03 14:40:05 +0200

    Change to match requirements for installing Oracle JDK

--- cloudera_cdh/bin/ubuntu/cloudera-install-jdk.sh
+++ cloudera_cdh/bin/ubuntu/cloudera-install-jdk.sh
@@ -1,6 +1,10 @@
 # Update apt-get
 sudo apt-get update -y
 
+#Install Software Properties Common to use apt-add-repository command
+sudo apt-get install software-properties-common python-software-properties -y
+sudo apt-get update -y
+
 # Install Oracle Java version for Ubuntu
 echo "Installing Oracle JDK"
-sudo apt-get install oracle-j2sdk1.7 -y
+sudo apt-get install oracle-java8-installer -y
